a1c71681da0a1818a12948c609dbdfe1de22648d
Saurav Shekhar <sauravshekhar01@gmail.com>
2017-06-21 17:20:59 +0200

    Old page added

--- index.md
+++ index.md
@@ -4,3 +4,4 @@ layout: default
 Welcome to my homepage. Not much to see here.
 
 [GSoC blog](blog/gsoc/index)
+[Old page](www/)
---
+++ www/resume/index.php
@@ -0,0 +1,10 @@
+<div id="resume">
+<object data="./resume.pdf" type="application/pdf" width="100%" height="100%">
+ 
+  <p>It appears you don't have a PDF plugin for this browser.
+  Please, <a href="./resume.pdf">click here to
+  download the PDF file.</a></p>
+  
+</object>
+
+</div>
